8069d63559acf0d57fe74fd62cb606b5aebdb022
Scott <scott.r.ja@gmail.com>
2021-07-14 22:25:37 -0500

    Initial commit

---
+++ src/test/kotlin/com/capstone2021/scouter/ScouterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.capstone2021.scouter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScouterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
