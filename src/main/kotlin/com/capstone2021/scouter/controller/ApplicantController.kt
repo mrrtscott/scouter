92adf7969143ceefc773a2baad912a69a9b0d001
Scott <scott.r.ja@gmail.com>
2021-07-25 17:34:57 -0500

    Applicant controller added.

--- src/main/kotlin/com/capstone2021/scouter/controller/ApplicantController.kt
+++ src/main/kotlin/com/capstone2021/scouter/controller/ApplicantController.kt
@@ -1,11 +1,29 @@
 package com.capstone2021.scouter.controller
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import com.capstone2021.scouter.model.Applicant
+import com.capstone2021.scouter.service.ApplicantService
+import com.capstone2021.scouter.service.CompanyService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/applicant")
 class ApplicantController {
+
+    @Autowired
+    lateinit var applicantService: ApplicantService
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun create(@RequestBody applicant:Applicant){
+        return applicantService.saveApplicant(applicant)
+    }
+
+    @GetMapping
+    fun findAll(): List<Applicant>?{
+        return applicantService.findAllApplicants()
+    }
 
 
 
